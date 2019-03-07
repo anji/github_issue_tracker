ff38f1715c258f8fd315e47c50881ed99002f538
Anjani <anjistic@gmail.com>
2019-03-07 18:38:49 +0530

    Adding comments
    Signed-off-by: Anjani <anjistic@gmail.com>

--- issue_diff.js
+++ issue_diff.js
@@ -1,6 +1,7 @@
 const request = require('request-promise')
-const package_url = require('url');
+const package_url = require('url')
 
+// Using request-promise package for getting json from url
 function get_data (git_link) {
   const options = {
     url: git_link,
@@ -12,18 +13,24 @@ function get_data (git_link) {
   return request(options)
 }
 
+// From the json data, iterate over each object to find the time of issue, and update each variable accordingly
 function get_issue_stat (words) {
+  // Filter out pull_request, as github issues api call included pull request
   words = words.filter(word => !('pull_request' in word))
-  let less_one_day = 0, betw_one_seven = 0, more_than_seven = 0
+
+  let less_one_day = 0; let betw_one_seven = 0; let more_than_seven = 0
   let total_issues = Object.keys(words).length
   console.log(Object.keys(words).length)
   for (let i = 0; i < Object.keys(words).length; i++) {
     let today = new Date()
     let then = new Date(words[i]['created_at'])
+    // calculate difference between current time and issue time in days
     let diff_days = (today - then) / 86400000
     if (diff_days <= 1) { less_one_day++ } else if (diff_days > 1 && diff_days <= 7) { betw_one_seven++ } else { break }
   }
   more_than_seven = (total_issues - less_one_day - betw_one_seven)
+
+  // Update response object with calculated values
   let issue_stat = {
     'total_issues': total_issues,
     'less_one_day': less_one_day,
@@ -36,20 +43,19 @@ function get_issue_stat (words) {
   return issue_stat
 }
 
+// Check URL for correct repository link
 function validate_url (url) {
-  const myURL = new URL(url);
-  console.log("anjani" + myURL.hostname);
-  console.log("anjani" + myURL.pathname);
+  const myURL = new URL(url)
 
-  if(myURL.hostname != "github.com")
-    return false;
+  // Check for hostname as github.com
+  if (myURL.hostname != 'github.com') { return false }
+
+  // Check for pathname to be in format "/user/repository/ "
   let re = new RegExp('(\/([a-zA-Z0-9\-]*)){2}(\/){0,1}$')
-  if (re.test(myURL.pathname))
-     return true 
-  else 
-    return false
+  if (re.test(myURL.pathname)) { return true } else { return false }
 }
 
+// Helper function to convert URL to API based URL
 function convert_to_api_url (url) {
   let u = url.split('github.com')
   let res = u[0] + 'api.github.com/repos' + u[1]
@@ -58,6 +64,7 @@ function convert_to_api_url (url) {
   return res
 }
 
+// Main function that fetches data for each available list of issues pages and updates total counter
 async function scrape_data (git_link) {
   let f_total_issues = 0
   let f_less_one_day = 0
@@ -67,11 +74,15 @@ async function scrape_data (git_link) {
   let page_link = git_link + page_num
   let break_flag = true
 
+  // Loop till end of number of pages of issue list
   do {
+    // wait for data to be fetched
     let raw_data = await get_data(page_link)
     console.log(page_link + ' ' + Object.keys(raw_data).length)
     if (Object.keys(raw_data).length === 0) {
       console.log('Empty page received')
+
+      // Break from the loop if empty response is received
       break_flag = false
     } else {
       let pg_stat = get_issue_stat(raw_data)
@@ -84,6 +95,7 @@ async function scrape_data (git_link) {
     }
   } while (break_flag)
 
+  // Return the final count as response object
   let fin_stat = {
     'f_total_issues': f_total_issues,
     'f_less_one_day': f_less_one_day,
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+// issue_diff.js includes all the utility function required for the project
 let issue = require('./issue_diff')
 const app = express()
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// Using pugjs template engine for HTML page rendering
 app.set('view engine', 'pug')
 
+// Setting port for the server to listen on
 let port = process.env.PORT || 8080
-// start the express web server listening on 8081
 app.listen(port, () => {
   console.log('listening on ' + port)
 })
@@ -19,20 +22,28 @@ app.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+// For displaying result of the fetched URL
 app.post('/results', (req, res) => {
   console.log('Checking Issue Stat for: ' + req.body.url_link)
   let git_link = req.body.url_link
 
+  // Parse URL to validate the fetched link
   if (!issue.validate_url(git_link)) {
     console.log('Invalid URL, Check Link')
-    res.render('error_page', {url: req.body.url_link})
+    res.render('error_page', { url: req.body.url_link })
     return
   }
+
+  // Convert user given url into API URL for github api requests
   git_link = issue.convert_to_api_url(git_link)
+
+  // Scrape data from the API URL
   issue.scrape_data(git_link).then(
     stats => {
-    res.render('results', {stats: stats, url: req.body.url_link})
-  }).catch(
-    (error) => res.render('error_page', {url: req.body.url_link}))
+    // Display results page using pugjs render engine
+      res.render('results', { stats: stats, url: req.body.url_link })
+    }).catch(
+    // Display error page using pugjs render engine
+    (error) => res.render('error_page', { url: req.body.url_link }))
   console.log('finished')
 })
